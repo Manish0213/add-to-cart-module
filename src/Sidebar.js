f610840213f938c8effd208882ece837a31dfa68
Manish0213 <ratnawatmanish031@gmail.com>
2024-03-26 22:57:12 -0700

    Initial commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,39 +1,15 @@
-import React, { useEffect, useState } from "react";
-import Cap1 from "./images/Cap1.jpg";
+import React from "react";
+// import Cap1 from "./images/Cap1.jpg";
 import "./Sidebar.css";
 
-const Sidebar = ({isVisible,handleToggleVisibility,cartItems,setCartItems,setInputValues,inputValues,updateSubtotal}) => {
+const Sidebar = ({isVisible,handleToggleVisibility,cartItems,addItemToCart}) => {
 
-  const handleQuantity = () => {
-    setInputValues([...inputValues, 0])
-  }
-
-  useEffect(()=>{
-    handleQuantity();
-  },[cartItems])
-
-  const handleIncrease = (index,cartItem) => {
-    // Increase the value of the input at the specified index by 1
-    const updatedValues = [...inputValues];
-    updatedValues[index] += 1;
-    setInputValues(updatedValues);
-    updateSubtotal(index, cartItem, updatedValues[index]);
+  const handleIncrease = (cartItem) => {
+    addItemToCart(cartItem.product._id,'increase');
   };
 
-  const handleDecrease = (index,cartItem) => {
-    // Decrease the value of the input at the specified index by 1
-    const updatedValues = [...inputValues];
-    updatedValues[index] -= 1;
-    setInputValues(updatedValues);
-    updateSubtotal(index, cartItem, updatedValues[index]);
-  };
-
-  const handleChange = (event, index, cartItem) => {
-    // Update the value of the input at the specified index whenever the input changes
-    const updatedValues = [...inputValues];
-    updatedValues[index] = parseInt(event.target.value);
-    setInputValues(updatedValues);
-    updateSubtotal(index, cartItem, updatedValues[index]);
+  const handleDecrease = (cartItem) => {
+    addItemToCart(cartItem.product._id,'decrease');
   };
 
   const grandTotal = cartItems.reduce((total, cartItem) => total + cartItem.subtotal, 0);
@@ -49,25 +25,25 @@ const Sidebar = ({isVisible,handleToggleVisibility,cartItems,setCartItems,setInp
                     <h5 className="card-title">Cart</h5>
                     <h5>
                       <i
-                        class="fa-solid fa-xmark"
+                        className="fa-solid fa-xmark"
                         onClick={handleToggleVisibility}
                       ></i>
                     </h5>
                   </div>
                   { cartItems.map((cartItem,index)=>(
-                    <div key={index} class="card mb-3" style={{ maxWidth: "540px" }}>
-                      <div class="row no-gutters">
-                        <div class="col-md-4">
-                          <img src={Cap1} class="card-img" alt="..." />
+                    <div key={index} className="card mb-3" style={{ maxWidth: "540px" }}>
+                      <div className="row no-gutters">
+                        <div className="col-md-4">
+                          <img src={`http://localhost:5000/uploads/${cartItem.product.imagePath}`} className="card-img" alt="..." style={{ }} height="114" width="300" />
                         </div>
-                        <div class="col-md-8">
-                          <div class="card-body">
-                            <h5 class="card-title">{cartItem.title}</h5>
+                        <div className="col-md-8">
+                          <div className="card-body">
+                            <h5 className="card-title">{cartItem.product.title}</h5>
                             <div className="d-flex justify-content-between">
                               <div>
-                              <button type="button" class="btn btn-dark" onClick={() => handleIncrease(index,cartItem)}>+</button>
-                              <input type='text' className="mx-2" value={inputValues[index]} onChange={(event) => handleChange(event, index, cartItem)} style={{ width: "30px", height: "30px" }}></input>
-                              <button type="button" class="btn btn-dark" onClick={() => handleDecrease(index,cartItem)}>-</button>
+                              <button type="button" className="btn btn-dark" onClick={() => handleIncrease(cartItem)}>+</button>
+                              <input type='text' className="mx-2" value={cartItem.quantity} style={{ width: "30px", height: "30px" }}></input>
+                              <button type="button" className="btn btn-dark" onClick={() => handleDecrease(cartItem)}>-</button>
                               </div>
                               <h5>${cartItem.subtotal}</h5>
                             </div>
